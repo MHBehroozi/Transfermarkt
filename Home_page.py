aabc73e81fd8a3e566d0210e9ef47c8daf061165
MHBehoozi <130773067+MHBehoozi@users.noreply.github.com>
2023-05-21 17:31:07 +0330

    Initial Streamlit scripts

---
+++ Home_page.py
@@ -0,0 +1,40 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title='Transfermarkt',
+    page_icon= ':soccer:'
+)
+st.title("Tranfermarkt DataAnalysis")
+
+"""
+    ### Introduction
+    Qera Data Analysis Bootcamp has been held for 10 weeks from April to June 2023. in which we learned and practiced both technical and soft skills in order to become ready to work as data analysts in the market and industry.
+
+    This webpage is for the first teamwork internship project of this Bootcamp, which is about data analysis in the field of football and includes extracting data from transfermarkt.com, storing data in a database and performing statistical analysis, and running machine learning models on delivered data.
+
+    ### Problem explanation
+    In this project, we do statistical analysis by collecting data from specific seasons of football leagues in different countries and answer some interesting questions by using machine learning tools.
+
+    This project includes 4 main phases:
+    * Data Extraction and Gathering
+    * Database Design
+    * Statistical Analysis
+    * Estimation and use of Machine Learning tools
+
+    See full problem explanation in persion on: [Wiki](https://github.com/MHBehoozi/Transfermarkt/wiki/Problem-Explanation)
+
+    ### Team members:
+    * [Mr. Behroozi, Mohammad Hoseyn](https://github.com/MHBehoozi)
+    * [Ms. Charghadchi, Mahla](https://github.com/mahla-chr)
+    * Mr. Hosseini, Amirreza
+    * [Mr. Mapar, Masoud](https://github.com/masoud533)
+    * [Ms. Mousavi, Fateme](https://github.com/fatememousavi97)
+    * Mr. Parsa, Ali
+
+    This team has completed the project under the mentoring of Mr. Abdolkarimi, Yousof.
+
+    GitHub Repository link:
+    [GitHub.com/MHBehoozi/Transfermarkt/](https://github.com/MHBehoozi/Transfermarkt/)
+
+"""
---
+++ pages/_3_Statistical_Analysis.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title='Transfermarkt',
+    page_icon= ':soccer:'
+)
+st.title("Tranfermarkt DataAnalysis")
