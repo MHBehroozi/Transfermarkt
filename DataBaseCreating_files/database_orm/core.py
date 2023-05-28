5249973e7b54080a029dc34dda599da88409a132
unknown <m.charghadchi@gmail.com>
2023-05-29 01:33:47 +0330

    Database ORM

--- DataBaseCreating_files/database_orm/core.py
+++ DataBaseCreating_files/database_orm/core.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy import URL
 
-from sqlalchemy import String, Integer, Column, ForeignKey, Float, Date, PrimaryKeyConstraint
+from sqlalchemy import String, Integer, Column, ForeignKey, Float, Date, PrimaryKeyConstraint, Enum
 from sqlalchemy.orm import relationship
 
 
@@ -72,13 +72,12 @@ class Award_winners(Base):
     award_id = Column(String(60))
     club_id = Column(Integer, ForeignKey('club.id'))
     win_year = Column(Integer)
-    # nation = Column(String(30))
 
     __table_args__ = (
             PrimaryKeyConstraint(club_id, award_id, win_year),
         )
     def __repr__(self) -> str:
-        return f"Award_winners(award_id= \n{self.award_id} \n cup_id= {self.cup_id}\n win_year= {self.win_year} \n nation= {self.nation})"
+        return f"Award_winners(award_id= \n{self.award_id} \n cup_id= {self.cup_id}\n win_year= {self.win_year} )"
 
 # Coach name,Appointed Date,id
 
@@ -114,6 +113,79 @@ class Stadium(Base):
     def __repr__(self) -> str:
         return f"Coach( club_id= {self.club_id}\n coach_name= {self.coach_name}\n appointed_date= {self.appointed_date} )"
 
+class Player(Base):
+    __tablename__ = "player"
+
+    # declare columns
+    id = Column(Integer, primary_key=True)
+    name = Column(String(60))
+    birth_date = Column(Date)
+    height = Column(Date)
+    main_position = Column(String(20))
+    foot = Column(Enum('left', 'right', 'both', name='player_foot'))
+    agent = Column(String(20))
+
+    def __repr__(self) -> str:
+        return f"Player( id= {self.id}\n name= {self.name}\n birth_date= {self.birth_date} \n height= {self.height} \n main_position= {self.main_position} \n foot= {self.foot} \n agent= {self.agent} )"
+
+class Player_statistics(Base):
+    __tablename__ = "player_statistics"
+
+    # declare columns
+    #player_id,season,Competition,club full name,Squad,Appearances,PPG,Goals,Own goals,Substitutions on,Substitutions off,Yellow cards,
+    # Second yellow cards,Red cards,Goals conceded,
+    # Clean sheets,Minutes played,Assists,Penalty goals,Minutes per goal,club_id,club name,market_value
+    player_id = Column(Integer)
+    club_id = Column(Integer)
+    season = Column(Integer)
+    competition = Column(String(60))
+    club_full_name = Column(String(20))
+    squad = Column(Integer)
+    appearances = Column(Integer)
+    PPG = Column(Float(2))
+    goals = Column(Integer)
+    own_goals = Column(Integer)
+    substitutions_on = Column(Integer)
+    substitutions_off = Column(Integer)
+    yellow_cards = Column(Integer)
+    second_yellow_cards = Column(Integer)
+    red_cards = Column(Integer)
+    goals_conceded = Column(Integer)
+    clean_sheets = Column(Integer)
+    minutes_played = Column(Float(3))
+    assists = Column(Integer)
+    penalty_goals = Column(Integer)
+    minutes_per_goal = Column(Integer)
+    club_name = Column(String(50))
+    market_value = Column(Float(precision='30,2'))
+
+    __table_args__ = (
+            PrimaryKeyConstraint(club_id, player_id, season, competition),
+        )
+
+    def __repr__(self) -> str:
+        return f"Player_statistics( player_id= {self.player_id}\n club_id= {self.club_id}\n season= {self.season} \n competition= {self.competition} \n squad= {self.squad} \n appearances= {self.appearances} \n PPG= {self.PPG} \n goals= {self.goals} \n own_goals= {self.own_goals} \n substitutions_on= {self.substitutions_on}\n substitutions_off= {self.substitutions_off}\n yellow_cards= {self.yellow_cards} \n second_yellow_cards= {self.second_yellow_cards} \n red_cards= {self.red_cards} \n goals_conceded= {self.goals_conceded} \n clean_sheets= {self.clean_sheets} \n minutes_played= {self.minutes_played} \n assists= {self.assists} \n penalty_goals= {self.penalty_goals} \n minutes_per_goal= {self.minutes_per_goal} \n market_value= {self.market_value} )"
+
+
+class Player_transfers(Base):
+    __tablename__ = "player_transfers"
+
+    # declare columns
+    # player_id,MV,Transfer_Fee,left,joined,Date,season,Loan_Fee,transfer/loan
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    player_id = Column(Integer)
+    MV = Column(Integer)
+    transfer_Fee = Column(Float(precision='10,2'))
+    left = Column(Integer())
+    joined = Column(String(20))
+    date = Column(Date)
+    season = Column(Integer)
+    loan_fee = Column(Float(2))
+    transfer_loan = Column(Integer)
+    
+   
+    def __repr__(self) -> str:
+        return f"Player_transfers( player_id= {self.player_id}\n MV= {self.MV}\n transfer_Fee= {self.transfer_Fee} \n left= {self.left} \n joined= {self.joined} \n date= {self.date} \n season= {self.season} \n loan_fee= {self.loan_fee} \n transfer_loan= {self.transfer_loan}  )"
 
 
 DB_NAME = 'quera_project1'
--- DataBaseCreating_files/database_orm/main.py
+++ DataBaseCreating_files/database_orm/main.py
@@ -15,6 +15,9 @@ df_club = pd.read_csv( os.path.join(dirname, "../preprocessing/final csv/Club_ta
 df_club_stats = pd.read_csv( os.path.join(dirname, "../preprocessing/final csv/Clubs_stats.csv"))
 df_coach = pd.read_csv( os.path.join(dirname, "../preprocessing/final csv/coach_df.csv"))
 df_stadium = pd.read_csv( os.path.join(dirname, "../preprocessing/final csv/Stadium_table.csv"))
+df_player = pd.read_csv( os.path.join(dirname, "../preprocessing/final csv/palyers.csv"))
+df_player_stats= pd.read_csv( os.path.join(dirname, "../preprocessing/final csv/palyers_statistic.csv"))
+df_player_transfers= pd.read_csv( os.path.join(dirname, "../preprocessing/final csv/transfer_players.csv"))
 
 with engine.connect() as connection :
  
@@ -32,6 +35,7 @@ with engine.connect() as connection :
     print('awards inserted to database')
 
     # award winners
+    df_award_winners.drop('nation', axis=1, inplace=True)
     df_award_winners.to_sql(name='award_winners', con=connection, schema='quera_project1', if_exists='append',\
                 index=False, method='multi', chunksize=12)
     connection.commit()
@@ -72,6 +76,61 @@ with engine.connect() as connection :
     connection.commit()
     print('stadium inserted to database')
 
+    # player
+    df_player = df_player.drop(df_player.columns[[0]], axis=1) # df.columns is zero-based pd.Index
+    df_player =df_player.drop_duplicates(subset=['player_id'])
+    df_player.rename(columns={ 
+    'player_id':'id',
+    }, inplace=True)
+    df_player.to_sql(name='player', con=connection, schema='quera_project1', if_exists='append',\
+                index=False, method='multi', chunksize=12)
+    connection.commit()
+    print('player inserted to database')
 
+
+
+    # player statistics
+    #player_id,season,Competition,club full name,Squad,Appearances,PPG,Goals,Own goals,Substitutions on,Substitutions off,Yellow cards,
+    # Second yellow cards,Red cards,Goals conceded,
+    # Clean sheets,Minutes played,Assists,Penalty goals,Minutes per goal,club_id,club name,market_value
+
+    df_player_stats.rename(columns={ 
+    'Competition':'competition',
+    'club full name':'club_full_name',
+    'Squad':'squad',
+    'Appearances':'appearances',
+    'Goals':'goals',
+    'Own goals':'own_goals',
+    'Substitutions on':'substitutions_on',
+    'Substitutions off':'substitutions_off',
+    'Yellow cards':'yellow_cards',
+    'Second yellow cards':'second_yellow_cards',
+    'Red cards':'red_cards',
+    'Goals conceded':'goals_conceded',
+    'Clean sheets':'clean_sheets',
+    'Minutes played':'minutes_played',
+    'Assists':'assists',
+    'Penalty goals':'penalty_goals',
+    'Minutes per goal':'minutes_per_goal',
+    'club name':'club_name',
+    }, inplace=True)
+
+    df_player_stats =df_player_stats.drop_duplicates(subset=['club_id', 'player_id', 'season', 'competition'])
+    df_player_stats.to_sql(name='player_statistics', con=connection, schema='quera_project1', if_exists='append',\
+                index=False, method='multi', chunksize=12)
+    connection.commit()
+    print('player statistics inserted to database')
+
+    # df_player_transfers
+    df_player_transfers.rename(columns={ 
+    'Transfer_Fee':'transfer_Fee',
+    'Date':'date',
+    'Loan_Fee':'loan_Fee',
+    'transfer/loan':'transfer_loan',
+    }, inplace=True)
+    df_player_transfers.to_sql(name='player_transfers', con=connection, schema='quera_project1', if_exists='append',\
+                index=False, method='multi', chunksize=12)
+    connection.commit()
+    print('player transfer inserted to database')
 
 
