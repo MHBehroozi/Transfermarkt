a32af6e60502a314865489d200a1f96ebf2a241c
MHBehroozi <hos.behroozi@gmail.com>
2023-05-31 04:21:18 +0330

    start working on statistical analysis page

--- pages/_3_Statistical_Analysis.py
+++ pages/_3_Statistical_Analysis.py
@@ -5,4 +5,10 @@ st.set_page_config(
     page_title='Transfermarkt',
     page_icon= ':soccer:'
 )
-st.title("Tranfermarkt DataAnalysis")
+st.title("Part 3: Statistical Analysis")
+"""
+In this part, we'd like to answer a number of questions with the help of statistical knowledge; Some questions have been asked to understand and find intuition from the data, others from the side of a particular person, and at the end a number of hypotheses have been proposed, and the validity of these hypotheses is examined in this section.
+"""
+
+st.header('Descriptive Statistics')
+'This section has 6 questions which need understanding the data and working with it. Also, it is necessary to draw diagrams to analyze the problems.'
